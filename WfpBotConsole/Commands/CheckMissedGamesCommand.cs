c23f8d1458688eeb9f3b6628b51a3082442e18bc
Yevhen Poliakov <poliakov.yevhen@gmail.com>
2021-01-09 16:42:12 +0200

    Add CheckMissedGamesCommand

---
+++ WfpBotConsole/Commands/CheckMissedGamesCommand.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Telegram.Bot;
+using Telegram.Bot.Types.Enums;
+using WfpBotConsole.DB;
+using WfpBotConsole.Models;
+
+namespace WfpBotConsole.Commands
+{
+	public class CheckMissedGamesCommand : Command
+	{
+		public override async Task Execute(long chatId, ITelegramBotClient client, GameRepository repository = null)
+		{
+			var lastGame = await repository.GetLastPlayedGame(chatId);
+
+			if (lastGame != default)
+			{
+				var gameDate = lastGame.PlayedAt;
+				var results = new List<string>();
+
+				while (gameDate.Date.AddDays(1) != DateTime.Today)
+				{
+					var users = await repository.GetAllPlayersAsync(chatId);
+
+					var newWinner = users[new Random().Next(users.Count)];
+
+					await repository.SaveGameResult(new GameResult()
+					{
+						ChatId = chatId,
+						UserId = newWinner.UserId,
+						UserName = newWinner.UserName,
+						PlayedAt = gameDate
+					});
+
+					results.Add($"*{gameDate:d}* - {newWinner.GetUserMention()}");
+				}
+
+				if (results.Any())
+				{
+					var msg = string.Format(Messages.MissedGames, string.Join(Environment.NewLine, results));
+					await client.SendTextMessageAsync(chatId, msg, ParseMode.Markdown);
+				}
+			}
+		}
+	}
+}
--- WfpBotConsole/Extensions.cs
+++ WfpBotConsole/Extensions.cs
@@ -2,9 +2,15 @@
 
 namespace WfpBotConsole
 {
-    public static class Extensions
-    {
-        public static string GetUserMention(this GameResult result)
-            => @$"[{result.UserName}](tg://user?id={result.UserId})";
-    }
+	public static class Extensions
+	{
+		public static string GetUserMention(this GameResult result)
+			=> CreateUserMention(result.UserName, result.UserId);
+
+		public static string GetUserMention(this Player player)
+			=> CreateUserMention(player.UserName, player.UserId);
+
+		private static string CreateUserMention(string userName, int userId)
+			=> @$"[{userName}](tg://user?id={userId})";
+	}
 }
