f72451583608d510ece4f16a83d3bf6a8cc37f14
PHermanov <germanovpavel@gmail.com>
2021-01-11 12:37:18 +0200

    log every message

--- WfpBotConsole/Program.cs
+++ WfpBotConsole/Program.cs
@@ -10,98 +10,96 @@ using System.IO;
 
 namespace WfpBotConsole
 {
-    class Program
-    {
-        private static ITelegramBotClient client;
-        private static GameRepository repository;
-        private static GameContext context;
+	class Program
+	{
+		private static ITelegramBotClient client;
+		private static GameRepository repository;
+		private static GameContext context;
 
-        static void Main(string[] args)
-        {
-            ConfigureServices();
-            SetUpJobs();
+		static void Main(string[] args)
+		{
+			ConfigureServices();
+			SetUpJobs();
 
-            var me = client.GetMeAsync().Result;
-            Console.WriteLine($"Bot started {me.Id} : {me.FirstName}");
+			var me = client.GetMeAsync().Result;
+			Console.WriteLine($"Bot started {me.Id} : {me.FirstName}");
 
-            client.StartReceiving();
+			client.StartReceiving();
 
-            Console.WriteLine("Type exit to exit");
+			Console.WriteLine("Type exit to exit");
 
-            while (true)
-            {
-                var input = Console.ReadLine();
+			while (true)
+			{
+				var input = Console.ReadLine();
 
-                if (input == "exit")
-                {
-                    client.StopReceiving();
-                    Console.WriteLine("Bot stopped");
+				if (input == "exit")
+				{
+					client.StopReceiving();
+					Console.WriteLine("Bot stopped");
 
-                    break;
-                }
-            }
-        }
+					break;
+				}
+			}
+		}
 
-        private static void ConfigureServices()
-        {
-            context = new GameContext();
-            repository = new GameRepository(context);
+		private static void ConfigureServices()
+		{
+			context = new GameContext();
+			repository = new GameRepository(context);
 
-            client = new TelegramBotClient(File.ReadAllText("key.secret"));
+			client = new TelegramBotClient(File.ReadAllText("key.secret"));
 
-            client.OnMessage += Bot_OnMessage;
-            client.OnReceiveError += Client_OnReceiveError;
-            client.OnReceiveGeneralError += Client_OnReceiveGeneralError;
-        }
+			client.OnMessage += Bot_OnMessage;
+			client.OnReceiveError += Client_OnReceiveError;
+			client.OnReceiveGeneralError += Client_OnReceiveGeneralError;
+		}
 
-        private static void SetUpJobs()
-        {
-            var getWinnerJob = new GetWinnerEverydayJob(repository, client);
-            JobManager.AddJob(getWinnerJob, s => s.ToRunEvery(1).Days().At(12, 00));
+		private static void SetUpJobs()
+		{
+			var getWinnerJob = new GetWinnerEverydayJob(repository, client);
+			JobManager.AddJob(getWinnerJob, s => s.ToRunEvery(1).Days().At(12, 00));
 
-            JobManager.Initialize();
-        }
+			JobManager.Initialize();
+		}
 
-        private static void Client_OnReceiveGeneralError(object sender, ReceiveGeneralErrorEventArgs e)
-        {
-            Console.WriteLine(e.Exception.Message);
-        }
+		private static void Client_OnReceiveGeneralError(object sender, ReceiveGeneralErrorEventArgs e)
+		{
+			Console.WriteLine(e.Exception.Message);
+		}
 
-        private static void Client_OnReceiveError(object sender, ReceiveErrorEventArgs e)
-        {
-            Console.WriteLine(e.ApiRequestException.Message);
-        }
+		private static void Client_OnReceiveError(object sender, ReceiveErrorEventArgs e)
+		{
+			Console.WriteLine(e.ApiRequestException.Message);
+		}
 
-        static async void Bot_OnMessage(object sender, MessageEventArgs e)
-        {
-            if (!e.Message.From.IsBot && e.Message.Type == MessageType.Text && !string.IsNullOrWhiteSpace(e.Message.Text))
-            {
-                var chatId = e.Message.Chat.Id;
-                var name = e.Message.From.Username;
-                var text = e.Message.Text;
+		static async void Bot_OnMessage(object sender, MessageEventArgs e)
+		{
+			if (!e.Message.From.IsBot && e.Message.Type == MessageType.Text && !string.IsNullOrWhiteSpace(e.Message.Text))
+			{
+				var chatId = e.Message.Chat.Id;
+				var userName = e.Message.From.Username;
+				var text = e.Message.Text;
 
-                var userName = e.Message.From.Username;
+				Console.WriteLine($"Received a message in chat {chatId}. {userName} : {text}");
 
-                if (string.IsNullOrWhiteSpace(userName))
-                {
-                    userName = e.Message.From.FirstName + " " + e.Message.From.LastName;
-                }
+				if (string.IsNullOrWhiteSpace(userName))
+				{
+					userName = e.Message.From.FirstName + " " + e.Message.From.LastName;
+				}
 
-                var newPlayer = await repository.CheckPlayer(chatId, e.Message.From.Id, userName);
+				var newPlayer = await repository.CheckPlayer(chatId, e.Message.From.Id, userName);
 
-                if (newPlayer)
-                {
-                    // await client.SendTextMessageAsync(chatId, string.Format(Messages.NewPlayerAdded, name));
-                    Console.WriteLine(string.Format(Messages.NewPlayerAdded, name));
-                }
+				if (newPlayer)
+				{
+					// await client.SendTextMessageAsync(chatId, string.Format(Messages.NewPlayerAdded, name));
+					Console.WriteLine(string.Format(Messages.NewPlayerAdded, userName));
+				}
 
-                if (text.StartsWith(@"/"))
-                {
-                    Console.WriteLine($"Received a command in chat {chatId}. {name} : {text}");
-
-                    await Command.Parse(text).Execute(e.Message.Chat.Id, client, repository);
-                }
-            }
-        }
-    }
+				if (text.StartsWith(@"/"))
+				{
+					await Command.Parse(text).Execute(e.Message.Chat.Id, client, repository);
+				}
+			}
+		}
+	}
 }
