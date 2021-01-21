b3cca6f059b1e88fc010f0a5a4f31ddd0ce85b50
PHermanov <germanovpavel@gmail.com>
2021-01-21 17:41:42 +0200

    typo fix

--- WfpBotConsole/Messages.Designer.cs
+++ WfpBotConsole/Messages.Designer.cs
@@ -106,7 +106,7 @@ namespace WfpBotConsole {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to Сегодня.
+        ///   Looks up a localized string similar to Сегодня .
         /// </summary>
         internal static string TodayString {
             get {
