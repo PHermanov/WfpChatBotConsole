f4789b7dde86cc1586cf3184797ba057c7abc35d
PHermanov <germanovpavel@gmail.com>
2021-01-26 22:40:14 +0200

    test command

--- WfpBotConsole/Commands/TestCommand.cs
+++ WfpBotConsole/Commands/TestCommand.cs
@@ -1,23 +1,30 @@
 ﻿using System.Threading.Tasks;
 using Telegram.Bot;
 using Telegram.Bot.Types.Enums;
+using WfpBotConsole.DB;
+using WfpBotConsole.Jobs;
 
 namespace WfpBotConsole.Commands
 {
 	public class TestCommand : ICommand
 	{
 		private readonly ITelegramBotClient _telegramBotClient;
+		private readonly IGameRepository _gameRepository;
 
 		public string CommandKey => "/test";
 
-		public TestCommand(ITelegramBotClient telegramBotClient)
+		public TestCommand(ITelegramBotClient telegramBotClient, IGameRepository gameRepository)
 		{
 			_telegramBotClient = telegramBotClient;
+			_gameRepository = gameRepository;
 		}
 
 		public async Task Execute(long chatId)
 		{
-			await _telegramBotClient.TrySendTextMessageAsync(chatId, $"Хуест!", parseMode: ParseMode.Markdown);
+			await _telegramBotClient.TrySendTextMessageAsync(chatId, $"Хуест!", ParseMode.Markdown);
+
+			var monthJob = new MonthWinnerJob(_gameRepository, _telegramBotClient);
+			await monthJob.Execute(chatId);
 		}
 	}
 }
