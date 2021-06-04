1ffd6506011154d53af2878f9f554720b84f01c1
PHermanov <germanovpavel@gmail.com>
2021-06-04 14:02:55 +0300

    Reply in 70% cases

--- WfpBotConsole/Services/AutoReplyService.cs
+++ WfpBotConsole/Services/AutoReplyService.cs
@@ -11,60 +11,65 @@ using WfpBotConsole.Resources;
 
 namespace WfpBotConsole.Services
 {
-	[Inject]
-	public class AutoReplyService : IAutoReplyService
-	{
-		private readonly ITelegramBotClient _telegramBotClient;
-		private readonly IGameRepository _gameRepository;
+    [Inject]
+    public class AutoReplyService : IAutoReplyService
+    {
+        private readonly ITelegramBotClient _telegramBotClient;
+        private readonly IGameRepository _gameRepository;
 
-		public AutoReplyService(
-			ITelegramBotClient telegramBotClient,
-			IGameRepository gameRepository)
-		{
-			_telegramBotClient = telegramBotClient;
-			_gameRepository = gameRepository;
-		}
+        public AutoReplyService(
+            ITelegramBotClient telegramBotClient,
+            IGameRepository gameRepository)
+        {
+            _telegramBotClient = telegramBotClient;
+            _gameRepository = gameRepository;
+        }
 
-		public async Task AutoReplyAsync(Message message)
-		{
-			var preparedText = new Regex("[^а-яА-Яa-zA-Z]")
-				.Replace(message.Text, string.Empty)
-				.ToLower();
+        public async Task AutoReplyAsync(Message message)
+        {
+            var preparedText = new Regex("[^а-яА-Яa-zA-Z]")
+                .Replace(message.Text, string.Empty)
+                .ToLower();
 
-			var answer = AutoReply.ResourceManager.GetString(preparedText);
+            var answer = AutoReply.ResourceManager.GetString(preparedText);
 
-			if (!string.IsNullOrEmpty(answer))
-			{
-				// multiple answers, pick random
-                if (answer.Contains(";"))
+            if (!string.IsNullOrEmpty(answer))
+            {
+                // replay 70% chance
+                if (new Random().Next(1, 101) <= 70)
                 {
-                    var answers = answer.Split(new[] {';'}, StringSplitOptions.RemoveEmptyEntries);
-                    answer = answers[new Random().Next(answers.Length)];
+                    // multiple answers, pick random
+                    if (answer.Contains(";"))
+                    {
+                        var answers = answer.Split(new[] { ';' }, StringSplitOptions.RemoveEmptyEntries);
+                        answer = answers[new Random().Next(answers.Length)];
+                    }
+
+                    await Task.Delay(TimeSpan.FromSeconds(1));
+                    await _telegramBotClient.TrySendTextMessageAsync(message.Chat.Id, answer, ParseMode.Markdown,
+                        replyToMessageId: message.MessageId);
                 }
+            }
+        }
 
-				await Task.Delay(TimeSpan.FromSeconds(1));
-				await _telegramBotClient.TrySendTextMessageAsync(message.Chat.Id, answer, ParseMode.Markdown, replyToMessageId: message.MessageId);
-			}
-		}
+        public async Task AutoMentionAsync(Message message)
+        {
+            var splitText = message.Text?.Split(new[] { " " }, StringSplitOptions.RemoveEmptyEntries);
 
-		public async Task AutoMentionAsync(Message message)
-		{
-			var splitText = message.Text?.Split(new[] { " " }, StringSplitOptions.RemoveEmptyEntries);
+            if (splitText.Any(w => w.Equals("@all", StringComparison.OrdinalIgnoreCase)))
+            {
+                var users = (await _gameRepository
+                    .GetAllPlayersAsync(message.Chat.Id))
+                    .Where(user => user.UserId != message.From.Id);
 
-			if (splitText.Any(w => w.Equals("@all", StringComparison.OrdinalIgnoreCase)))
-			{
-				var users = (await _gameRepository
-					.GetAllPlayersAsync(message.Chat.Id))
-					.Where(user => user.UserId != message.From.Id);
+                if (users.Any())
+                {
+                    var answer = $"\U0001F51D {users.GetUsersMention()}";
 
-				if (users.Any())
-				{
-					var answer = $"\U0001F51D {users.GetUsersMention()}";
-
-					await Task.Delay(TimeSpan.FromSeconds(1));
-					await _telegramBotClient.TrySendTextMessageAsync(message.Chat.Id, answer, ParseMode.Markdown, replyToMessageId: message.MessageId);
-				}
-			}
-		}
-	}
+                    await Task.Delay(TimeSpan.FromSeconds(1));
+                    await _telegramBotClient.TrySendTextMessageAsync(message.Chat.Id, answer, ParseMode.Markdown, replyToMessageId: message.MessageId);
+                }
+            }
+        }
+    }
 }
